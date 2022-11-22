c215df5c124027f38f57b4ab6029c6c0fd722810
user <user@WIN-8I41D9CN0BC>
2022-11-22 08:39:05 +0300

    Добавьте файлы проекта.

---
+++ Array1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 7;
+            float S = 0;
+            int[] array = new int[n];
+            Console.WriteLine("Введите числа");
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                    S += array[i];
+                }
+                Console.WriteLine("Среднее арифметическое = {0:f2} ", S/n);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Array5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Введите целочисленное значение");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n, n];
+            {
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (((i % 2 == 0) && (j % 2 == 0)) || (i % 2 != 0) && (j % 2 != 0))
+                            array[i, j] = 1; 
+
+                Console.Write("{0,4} ", array[i, j]);
+
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
